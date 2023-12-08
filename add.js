249aa0b4d9fbffc61bf81402410317f5e02cfe47
anshuman <anshuman@gmail.com>
2023-12-08 12:15:37 +0530

    add file added to repo

---
+++ add.js
@@ -0,0 +1,11 @@
+function add() {
+
+let first = document. getElementById("first").value
+let second = document. getElementById("second").value
+
+
+let result = first + second
+
+document.getElementById("result").value = result
+
+}
